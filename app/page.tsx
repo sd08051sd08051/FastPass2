efbb8a2be7ce4e5c266cca02fc59491ac01d718a
sd08051sd08051 <southafrica.2018.durban@gmail.com>
2024-01-27 18:52:16 +0900

    コメント

--- app/page.tsx
+++ app/page.tsx
@@ -7,12 +7,15 @@ import { getAllBooks } from "./lib/next-auth/microcms/client";
 import { nextAuthOptions } from "./lib/next-auth/options";
 import { BookType } from "./types/types";
 
+// 非同期処理で実施
 export default async function Home() {
+  // 本のデータを取得
   const { contents } = await getAllBooks();
   // console.log(contents);
+  // セッション情報の取得をして認証する
   const session = await getServerSession(nextAuthOptions);
   const user: any = session?.user;
-
+  // ユーザーがログインしている場合fetchで特定のAPIエンドポイントに行く
   if (user) {
     const response = await fetch(
       `${process.env.NEXT_PUBLIC_API_URL}/purchases/${user.id}`
@@ -20,10 +23,12 @@ export default async function Home() {
   }
   return (
     <>
+      {/* Tailwind CSS スタイルクラス */}
       <main className="flex flex-wrap justify-center items-center md:mt-32 mt-20">
         <h2 className="text-center w-full font-bold text-3xl mb-2">
           Ramen Fast Pass
         </h2>
+        {/* JSX内でJSを入れ込みNookコンポーネント配列を生成 */}
         {contents.map((book: BookType) => (
           <Book key={book.id} book={book} />
         ))}
